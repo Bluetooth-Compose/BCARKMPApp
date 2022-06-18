e1a11c9e01a526e67b871b240888e7eaefc843c6
Super Admin (Anthony Harrison) <super.admin@bluetooth-compose.com>
2022-06-18 16:52:44 +0100

    chore(framework): converted from xml activity ui to jetpack compose, using default compose activity project as a guide

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,3 +1,6 @@
+
+// NOTE: Jetpack Compose was added by following this guide: https://developer.android.com/jetpack/compose/interop/adding but catered for kotlin syntax instead of groovy syntax
+
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -12,6 +15,20 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+    buildFeatures {
+        // Enables Jetpack Compose for this module
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
@@ -24,4 +41,19 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.4.0")
+    // Compose Material Design
+    implementation("androidx.compose.material:material:1.1.1")
+    // Animations
+    implementation("androidx.compose.animation:animation:1.1.1")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    // Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+    // UI Tests
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
+    // When using a AppCompat theme
+    implementation("com.google.accompanist:accompanist-appcompat-theme:0.16.0")
 }
--- androidApp/src/main/java/com/bluetoothcompose/apps/bcarapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/bluetoothcompose/apps/bcarapp/android/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.bluetoothcompose.apps.bcarapp.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.bluetoothcompose.apps.bcarapp.Greeting
-import android.widget.TextView
+import com.bluetoothcompose.apps.bcarapp.android.ui.theme.BCARAppTheme
 
-fun greet(): String {
-    return Greeting().greeting()
-}
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            BCARAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BCARAppTheme {
+        Greeting("Android")
     }
 }
